92217cfd036d32975b93683dc944ac3853e76c43
OperatorIO-stock <vova.chigorin@yandex.com>
2024-11-24 16:11:36 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ProgramCycle;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const string CommandExitProgram = "/exit";
+
+        string messageUser;
+        bool isWorkProgram = true;
+
+        Console.WriteLine("Чтобы выйти из программы - /exit");
+
+        while(isWorkProgram)
+        {
+            messageUser = Console.ReadLine();
+
+            if (messageUser == CommandExitProgram)
+            {
+                isWorkProgram = false;
+            }
+        }
+
+    }
+}
